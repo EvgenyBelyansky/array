47f4ec9c8f46935bd3e66761a8963597b51003af
EvgenyBelyansky <e.g.belyansky@gmail.com>
2024-04-29 02:08:27 +0400

    Урок №2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
 public class Main {
     public static void main(String[] args) {
+//        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+//        int[] weightsCopy = {901, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+//        int januaryWeight = weights[0];
+//        System.out.println(januaryWeight);
+//        System.out.println(weights[0]);
+//        System.out.println(weights[4]);
+//        int january = 0;
+//        System.out.println(weights[january]);
+//        for (int i = 0; i < weights.length; i++) {
+//            System.out.println(weights[i]);
+//        }
+//        boolean arraysAreEqal = true;
+//        arraysAreEqal = weights.length == weightsCopy.length;
+//        if (arraysAreEqal) {
+//            for (int i = 0; i < weights.length; i++) {
+//                if (weights[i] != weightsCopy[i]) {
+//                    arraysAreEqal = false;
+//                }
+//            }
+//        }
+//        if (arraysAreEqal) {
+//            System.out.println("Массивы одинаковые");
+//        } else {
+//            System.out.println("Массивы разныее");
+//        }
+//        int maxWeight = -1;
+//        for (final int current : weights) {
+//            if (current > maxWeight) {
+//                maxWeight = current;
+//            }
+//        }
+//        System.out.println(maxWeight);
+//
+//        for (int i = 0; i < weights.length - 1 && weights[i + 1] != 0; i++) {
+//            System.out.println(weights[i + 1] - weights[i]);
+//        }
+
+
+//        int[] rack = new int[4];
+//        rack[0] = 12;
+//        rack[1] = 9;
+//        rack[2] = 11;
+//        rack[3] = 10;
+//        int[] rack2 = {12, 9, 11, 10};
+//        boolean rackAreEqal = true;
+//        rackAreEqal = rack.length == rack2.length;
+//        if (rackAreEqal) {
+//            for (int i = 0; i < rack.length; i++) {
+//                if (rack[i] != rack2[i]) {
+//                    rackAreEqal = false;
+//                }
+//            }
+//        }
+//        if (rackAreEqal) {
+//            System.out.println("Стелажи одинаковые");
+//        } else {
+//            System.out.println("Стелажи разные");
+//        }
+
 
     }
 }
